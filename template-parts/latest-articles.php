9565fac48c18efdfa77b6b487010a3476f739896
Nemanja Blagojevic <nemanja@darwindigital.com>
2020-10-09 17:10:33 +0200

    Update loop

--- template-parts/latest-articles.php
+++ template-parts/latest-articles.php
@@ -4,27 +4,33 @@
             <h2 class="h2 latest-articles__title">Latest Articles</h2>
 
             <ul>
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                    <li class="article-card">
-                        <a href="<?php the_permalink(); ?>">
-                            <h3 class="h3"><?php the_title(); ?></h3>
-                        </a>
-                        <date class="date article-card__date"><?php echo get_the_date(); ?></date>
-                        <p><?php the_excerpt(); ?></p>
-                        <a class="button button--primary article-card__button" href="<?php the_permalink(); ?>">Read more</a>
-                        <?php if ( has_post_thumbnail() ) : ?>
-                            <div class="article-card__image">
-                                <?php the_post_thumbnail('medium'); ?>
-                            </div>
-                        <?php endif; ?>
-                    </li>
-                <?php endwhile; else : ?>
-                    <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-                <?php endif; ?>
-                
+            <?php $lastposts = get_posts( array(
+                        'posts_per_page' => 3
+                    ) );
 
-
+                    if ( $lastposts ) {
+                        foreach ( $lastposts as $post ) :
+                            setup_postdata( $post ); ?>
+                            <li class="article-card">
+                                <a href="<?php the_permalink(); ?>">
+                                    <h3 class="h3"><?php the_title(); ?></h3>
+                                </a>
+                                <date class="date article-card__date"><?php echo get_the_date(); ?></date>
+                                <p><?php the_excerpt(); ?></p>
+                                <a class="button button--primary article-card__button" href="<?php the_permalink(); ?>">Read more</a>
+                                <?php if ( has_post_thumbnail() ) : ?>
+                                    <div class="article-card__image">
+                                        <?php the_post_thumbnail('medium'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </li>
+                        <?php
+                        endforeach; 
+                        wp_reset_postdata();
+                    }
+                ?>
             </ul>
+
 
         </div>
     </div>

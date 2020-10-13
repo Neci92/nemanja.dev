d6d5868b2ade09c46c701a505b1d0b2043d4ceef
Nemanja Blagojevic <nemanja@darwindigital.com>
2020-10-13 16:26:58 +0200

    Minor layout changes

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#171819">
+    <meta name="msapplication-navbutton-color" content="#171819">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#171819">
     <title><?php wp_title("|", true, "right"); ?>Nemanja.dev </title>
 </head>
 <body>

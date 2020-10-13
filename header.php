6c6b9ab82c0d3317b2cf8f18e2f6290b7fc3242f
Nemanja Blagojevic <nemanja@darwindigital.com>
2020-10-13 16:18:05 +0200

    Add mobile color

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#171819">
     <title><?php wp_title("|", true, "right"); ?>Nemanja.dev </title>
 </head>
 <body>

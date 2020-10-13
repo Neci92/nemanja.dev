40e17c0d76c249aae8b2e5f1b8a3efe87f3d5737
Nemanja Blagojevic <nemanja@darwindigital.com>
2020-10-13 16:10:38 +0200

    Small changes on homepage

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WordPress theme development</title>
+    <title><?php wp_title("|", true, "right"); ?>Nemanja.dev </title>
 </head>
 <body>
 
--- template-parts/intro.php
+++ template-parts/intro.php
@@ -5,6 +5,7 @@
                 <h1 class="h1 intro__header"><?php the_title(); ?></h1>
                 <div class="intro__text">
                     <?php the_content(); ?>
+                    <a class="button button--primary intro__button" href="#">Contact me</a>
                 </div>
             </div>
             <div class="intro__image">

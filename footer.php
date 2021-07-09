7184cee6062a0b612b0c6490d51d26a45aa9f515
Nemanja Blagojevic <nemanja@darwindigital.com>
2021-07-09 23:00:59 +0200

    Add social network to footer

--- footer.php
+++ footer.php
@@ -2,27 +2,33 @@
 <footer data-module="FOOTER" class="footer">
     <div class="container">
         <div class="footer__wrapper">
-            <p class="footer__message">Blagojevic Nemanja 2021.</p>
-            <!-- <ul class="navigation__list">
+            <!-- <p class="footer__message">Blagojevic Nemanja 2021.</p> -->
+            <ul class="navigation__list">
                 <li class="navigation__list-item">
                     <a class="button button--icon" href="#">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/User icon.svg" alt="User icon">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/instagram.svg" alt="User icon">
                     <span class="navigation__link-text navigation__link-text--footer">Instagram</span>
                     </a>
                 </li>
                 <li class="navigation__list-item">
                     <a class="button button--icon" href="#">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Loupe button.svg" alt="Search icon">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/linkedin.svg" alt="Search icon">
                     <span class="navigation__link-text navigation__link-text--footer">Linkedin</span>
                     </a>
                 </li>
                 <li class="navigation__list-item">
                     <a class="button button--icon" href="#">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Mail button.svg" alt="Contact me">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/github.svg" alt="Contact me">
                     <span class="navigation__link-text navigation__link-text--footer">Github</span>
                     </a>
                 </li>
-            </ul> -->
+                <li class="navigation__list-item">
+                    <a class="button button--icon" href="#">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/twitter.svg" alt="Contact me">
+                    <span class="navigation__link-text navigation__link-text--footer">Twitter</span>
+                    </a>
+                </li>
+            </ul>
 
             <?php include("template-parts/navigation.php"); ?>
         </div>

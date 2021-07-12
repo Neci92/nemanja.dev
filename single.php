0c2c8657b8b933e43264ec711b3976e39d511d99
Nemanja Blagojevic <nemanja@darwindigital.com>
2021-07-12 12:48:20 +0200

    Highway changes

--- single.php
+++ single.php
@@ -2,6 +2,7 @@
 
     <div data-router-view="single">
         <?php require('template-parts/blog-post.php'); ?>
+        <a href="<?php echo home_url(); ?>">HOME</a>
     </div>
 
 <?php get_footer(); ?>

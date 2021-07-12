da00cb78b4788ff8a9caa18ab4ed0e69760a9a40
Nemanja Blagojevic <nemanja@darwindigital.com>
2021-07-12 16:23:34 +0200

    Set blog to max width 76rem

--- template-parts/blog-post.php
+++ template-parts/blog-post.php
@@ -1,8 +1,10 @@
 <section class="blog-post">
     <div class="container">
-        <div class="blog-post__wrapper box-wrapper">
-            <h1 class="h1 mb blog-post__title"><?php the_title(); ?></h1>
-            <?php the_content(); ?>
+        <div class="box-wrapper">
+            <div class="blog-post__content">
+                <h1 class="h1 mb blog-post__title"><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+            </div>
         </div>
     </div>
 </section>
